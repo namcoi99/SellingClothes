48583f4c64ca3522b588fc435aa1c5afb406a703
Khanh Ly <khanhlyvu30@gmail.com>
2020-12-07 23:50:45 +0700

    feat: Create, update, delete API for product

--- backend/middleware/checkLogIn.js
+++ backend/middleware/checkLogIn.js
@@ -1,6 +1,6 @@
 module.exports = function isLoggedIn(req, res, next) {
     if (!req.session.currentUser) {
-        return res.status(500).json({
+        return res.status(401).json({
             success: false,
             message: "Please login before doing this action"
         });
